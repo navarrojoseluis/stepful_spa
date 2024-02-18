606674a1573bc4c65f49f9d33ff4cb704c677da5
Jose Luis Navarro <44554125+navarrojoseluis@users.noreply.github.com>
2024-02-18 16:12:52 -0500

    chore: get user from url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
-function App() {
-  return <div className="App">Hello World</div>;
-}
+const App = (): JSX.Element => {
+  const [userId, setUserId] = useState<string | null>(null);
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    setUserId(searchParams.get("id"));
+  }, [searchParams]);
+
+  if (!userId)
+    return (
+      <p>
+        Please provide a valid user ID in the URL parameters. Example:
+        "http://localhost:3001/?id=1"
+      </p>
+    );
+
+  return <div className="App">Hello user {userId}</div>;
+};
 
 export default App;
